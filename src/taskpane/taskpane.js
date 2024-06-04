01ecd050bc9e7ec3add091d4519776eba0971427
Jhonny Blanco <rodri_n@hotmail.es>
2024-06-03 20:13:16 -0400

    adding iadb content to page

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -17,6 +17,13 @@ export async function run() {
   /**
    * Insert your Outlook code here
    */
+  var el = document.createElement("iframe");
+  el.src = 'https://iadbdev.service-now.com/x_nuvo_eam_fm_view_v2.do?app=user#?s=60a9c6c31b4460504e9886e9cd4bcbe1&search=aca9c6c31b4460504e9886e9cd4bcbe0&view=default&label_size=8&qr=true';
+  el.id = 'miIframe';
+  document.getElementById("miIframe")?.remove();
+  document.getElementById("preview").appendChild(el);
+
+
 
   const item = Office.context.mailbox.item;
   document.getElementById("item-subject").innerHTML = "<b>Subject:</b> <br/>" + item.subject;
