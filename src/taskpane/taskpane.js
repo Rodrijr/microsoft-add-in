f4dea545b9ac187cdd486cf55d08c05cf73deb94
Jhonny Blanco <rodri_n@hotmail.es>
2024-06-20 11:15:54 -0400

    get location sys id

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -25,24 +25,35 @@ Office.onReady((info) => {
   console.log('Office.onReady')
   run();
 });
-
+function getLocationCode(input) {
+  const parts = input.split(' - ');
+  if (parts.length >= 2) {
+    return parts[1];
+  }
+  return null;
+}
 async function run() {
-  console.log('AAAAAAAAAAAAAAAAAQQQQQQQQQQQQQQQUIIIIIIIIIIIIIIII');
-  instance.get('now/table/x_nuvo_eam_elocation?sysparm_fields=sys_id&sysparm_limit=1&location_code=NE0C31')
-    .then((response) => {
-      console.log('>>>>>', response);
-    }).catch(error => {
-      console.log('error >>>>>>>>>', error);
-    });
-
-  var el = document.createElement("iframe");
-  el.src = 'https://iadbdev.service-now.com/x_nuvo_eam_fm_view_v2.do?app=user#?s=60a9c6c31b4460504e9886e9cd4bcbe1&search=aca9c6c31b4460504e9886e9cd4bcbe0&view=default&label_size=8&qr=true';
-  el.id = 'miIframe';
-  el.referrerpolicy = "strict-origin-when-cross-origin";
-  document.getElementById("miIframe")?.remove();
-  document.getElementById("preview").appendChild(el);
-  const item = Office.context.mailbox.item;
-  document.getElementById("item-subject").innerHTML = "<b>Subject:</b> <br/>" + item.subject;
+  try {
+    const item = Office.context.mailbox.item;
+    const subject = item.subject;
+    const locationCode = getLocationCode(subject);
+    if (locationCode) {
+      var { data } = await instance.get('now/table/x_nuvo_eam_elocation?sysparm_fields=sys_id&sysparm_limit=1&location_code=' + locationCode)
+      console.log('>>>>>', data[0]);
+      if (data && data[0]) {
+        var sys_id = data[0].sys_id
+        var el = document.createElement("iframe");
+        el.src = 'https://iadbdev.service-now.com/x_nuvo_eam_fm_view_v2.do?app=user#?search=' + sys_id;
+        el.id = 'miIframe';
+        el.referrerpolicy = "strict-origin-when-cross-origin";
+        document.getElementById("miIframe")?.remove();
+        document.getElementById("preview").appendChild(el);
+        const item = Office.context.mailbox.item;
+      }
+    }
+  } catch (error) {
+    console.log('error >>>>>>>>>', error);
+  }
 }
 run();
 console.log('AAAAAAAAAAAAAFUERA');
