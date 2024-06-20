fa4cf4698f252fe6c3f69a6b9fae2c86af9aa64a
Jhonny Blanco <rodri_n@hotmail.es>
2024-06-20 09:26:56 -0400

    creating request

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -4,6 +4,7 @@
  */
 
 /* global document, Office */
+import axios from 'axios';
 
 Office.onReady((info) => {
   if (info.host === Office.HostType.Outlook) {
@@ -11,19 +12,34 @@ Office.onReady((info) => {
   }
 });
 
+const instance = axios.create({
+  baseURL: 'https://some-domain.com/api/',
+  timeout: 1000,
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    "X-UserToken": "4ad5b6131b3246d4797f0fa4cc4bcbc5de0c58004311df144973f9c68b5405ab997284f7"
+  }
+});
+
 export async function run() {
   /**
    * Insert your Outlook code here
    */
+  instance.get('https://iadbdev.service-now.com/api/now/table/x_nuvo_eam_elocation?sysparm_fields=sys_id&sysparm_limit=1&location_code=NE0C31')
+    .then((response) => {
+      console.log('>>>>>', response)
+    }).catch(error => {
+      console.log('error >>>>>>>>>', error)
+    })
+
+
   var el = document.createElement("iframe");
   el.src = 'https://iadbdev.service-now.com/x_nuvo_eam_fm_view_v2.do?app=user#?s=60a9c6c31b4460504e9886e9cd4bcbe1&search=aca9c6c31b4460504e9886e9cd4bcbe0&view=default&label_size=8&qr=true';
   el.id = 'miIframe';
   el.referrerpolicy = "strict-origin-when-cross-origin";
   document.getElementById("miIframe")?.remove();
   document.getElementById("preview").appendChild(el);
-
-
-
   const item = Office.context.mailbox.item;
   document.getElementById("item-subject").innerHTML = "<b>Subject:</b> <br/>" + item.subject;
 }
