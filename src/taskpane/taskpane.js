00eb6518c3cb1d26952dbd5271b4a7db9ad6ce55
Jhonny Blanco <rodri_n@hotmail.es>
2024-06-20 09:54:45 -0400

    debug

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -2,10 +2,7 @@
  * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
  * See LICENSE in the project root for license information.
  */
-
 /* global document, Office */
-import axios from 'axios';
-
 console.log('AAAAAAAAAAAAAFUERA 1')
 
 const instance = axios.create({
