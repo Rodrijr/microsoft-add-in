b43ff9d71989d9f135b3c5b4ad81063ac2e804c2
Jhonny Blanco <rodri_n@hotmail.es>
2024-06-20 11:35:14 -0400

    without destructuring

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -42,7 +42,7 @@ async function run() {
     if (locationCode) {
       var response= await instance.get('now/table/x_nuvo_eam_elocation?sysparm_fields=sys_id&sysparm_limit=1&location_code=' + locationCode)
       console.log('JRBP -> response:', response);
-      var { data } = response;
+      var  data = response.data;
       console.log('>>>>>', data[0]);
       if (data && data[0]) {
         var sys_id = data[0].sys_id
