b6af5c0756e6ccfe110b5655bb429246db94285a
Jhonny Blanco <rodri_n@hotmail.es>
2024-08-16 16:57:03 -0400

    auth token

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -6,7 +6,6 @@ Office.onReady((info) => {
     initialize();
   }
 });
-
 const instance = axios.create({
   baseURL: 'https://iadbdev.service-now.com/api/',
   timeout: 5000,
@@ -17,24 +16,24 @@ const instance = axios.create({
   }
 });
 
-// Obtener el token de autenticación básica
-const authToken = 'Basic ' + btoa('autocad_integration' + ':' + 'AutoCadIntegration67=');
-
 function subjectCB(result) {
   return result;
 }
-
 async function initialize() {
+
   if (Office.context.mailbox && Office.context.mailbox.item) {
+    console.log('JRBP -> Office.context.mailbox:', Office.context.mailbox);
+    console.log('JRBP ->  Office.context.mailbox.item:', Office.context.mailbox.item);
     let item = Office.context.mailbox.item;
     let sub = '';
-
     if (typeof item.subject == 'string') {
-      sub = item.subject;
+      sub = item.subject
     } else {
-      sub = item.subject.getAsync().value;
+      sub = item.subject.getAsync()
+      sub = sub.value;
     }
 
+    console.log('JRBP -> sub:', sub);
     const subject = 'NE1075' || getLocationCode(sub);
 
     if (subject) {
@@ -67,19 +66,22 @@ function getUserIdentityToken() {
 async function establishServiceNowSession(locationCode) {
   if (locationCode) {
     try {
-      await getUserIdentityToken();
-      const sys_id = await getLocationID(locationCode);
+      // getUserIdentityToken()
+
+      var sys_id = await getLocationID(locationCode);
     } catch (error) {
       console.error('Error establishing session with ServiceNow:', error.response.status);
       console.error('Error establishing session with ServiceNow:', error.response.statusText);
-
       Office.context.ui.displayDialogAsync('https://iadbdev.service-now.com/x_nuvo_eam_fm_view_v2.do?app=user#?s=e2a369cd47dee5d08aba7f67536d4387&view=default&search=' + sys_id,
         { height: 45, width: 55 },
         function (asyncResult) {
+          Office.context.ui.messageParent(token);
           if (asyncResult.status === Office.AsyncResultStatus.Failed) {
+            // Show an error message
             console.log('Failed to open dialog: ' + asyncResult.error.message);
           } else {
-            const dialog = asyncResult.value;
+            // getUserIdentityToken()
+            var dialog = asyncResult.value;
             dialog.addEventHandler(Office.EventType.DialogMessageReceived, function (args) {
               console.log('Message received from dialog: ' + args.message);
             });
@@ -92,35 +94,29 @@ async function establishServiceNowSession(locationCode) {
     }
   }
 }
+const authToken = 'Basic ' + btoa('autocad_integration' + ':' + 'AutoCadIntegration67=');
 
 async function getLocationID(locationCode) {
-  const response = await instance.get(`now/table/x_nuvo_eam_elocation?sysparm_fields=sys_id&sysparm_limit=1&location_code=${locationCode}`);
-  const data = response.data?.result;
-
+  var response = await instance.get('now/table/x_nuvo_eam_elocation?sysparm_fields=sys_id&sysparm_limit=1&location_code=' + locationCode);
+  console.log('JRBP -> response:', response);
+  var data = response.data?.result;
+  console.log('>>>>> 1 ', data[0]);
   if (data && data[0]) {
-    const sys_id = data[0].sys_id;
-    const iframeUrl = `https://iadbdev.service-now.com/x_nuvo_eam_fm_view_v2.do?app=user#?s=e2a369cd47dee5d08aba7f67536d4387&view=default&search=${sys_id}`;
 
-    // Aquí puedes abrir la página de ServiceNow en un iframe o en un diálogo modal
-    openServiceNowPage(iframeUrl, authToken);
-
-    return sys_id;
+    var sys_id = data[0].sys_id;
+    var el = document.createElement("iframe");
+    el.src = 'https://iadbdev.service-now.com/x_nuvo_eam_fm_view_v2.do?app=user#?s=e2a369cd47dee5d08aba7f67536d4387&view=default&search=' + sys_id;
+    console.log('JRBP -> el.src:', el.src);
+    el.id = 'miIframe';
+    el.sandbox = " allow-scripts allow-same-origin";
+    el.referrerpolicy = "strict-origin-when-cross-origin";
+    el.setAttribute('Authorization', authToken);
+    var a = document.getElementById("miIframe")?.remove();
+    document.getElementById("preview").appendChild(el);
+    //window.location.replace('https://iadbdev.service-now.com/x_nuvo_eam_fm_view_v2.do?app=user#?s=e2a369cd47dee5d08aba7f67536d4387&view=default&search=' + sys_id);
+    // document.location =
   }
+  return sys_id;
 }
 
-function openServiceNowPage(url, authToken) {
-  const el = document.createElement('iframe');
-  el.src = url;
-  el.id = 'miIframe';
-  el.sandbox = 'allow-scripts allow-same-origin';
-  el.referrerpolicy = 'strict-origin-when-cross-origin';
-  el.setAttribute('Authorization', authToken);
-
-  const preview = document.getElementById('preview');
-  if (preview) {
-    preview.innerHTML = '';
-    preview.appendChild(el);
-  } else {
-    document.body.appendChild(el);
-  }
-}
+Office.actions.associate("action", initialize);
