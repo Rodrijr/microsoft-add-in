6859ba4a25aca0edafb542bc722fa70ec0bba4fa
Jhonny Blanco <rodri_n@hotmail.es>
2024-08-28 12:32:22 -0400

    login

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -25,17 +25,32 @@ function subjectCB(result) {
 }
 
 async function initialize() {
-  window.location = "https://iadbdev.service-now.com/login.do";
-  document.addEventListener("DOMContentLoaded", function (event) {
-    var user = document.getElementById("user_name")
-    user.setValue('autocad_integration')
-    var pass = document.getElementById("user_password")
-    pass.setValue('AutoCadIntegration67=')
-    sysverb_login
-    var button = document.getElementById("sysverb_login")
-    button.click();
-    window.location = "https://iadbdev.service-now.com/x_nuvo_eam_fm_view_v2.do?app=user#?s=e2a369cd47dee5d08aba7f67536d4387&view=default&search=";
-   })
+  const el = document.createElement('iframe');
+  el.src = "https://iadbdev.service-now.com/login.do";
+  el.id = 'miIframe';
+  el.sandbox = "allow-scripts allow-forms allow-same-origin allow-popups allow-popups-to-escape-sandbox allow-modals allow-downloads allow-storage-access-by-user-activation";
+  el.className = "AddinIframe";
+
+  document.getElementById("miIframe")?.remove();
+  document.getElementById("preview").appendChild(el);
+
+  el.onload = function () {
+    const iframeWindow = el.contentWindow;
+    const iframeDocument = el.contentDocument || iframeWindow.document;
+
+    if (iframeWindow.location.href.includes("/login.do")) {
+      const user = iframeDocument.getElementById("user_name");
+      user.value = 'autocad_integration';
+
+      const pass = iframeDocument.getElementById("user_password");
+      pass.value = 'AutoCadIntegration67=';
+
+      const button = iframeDocument.getElementById("sysverb_login");
+      button.click();
+
+      iframeWindow.location.href = "https://iadbdev.service-now.com/x_nuvo_eam_fm_view_v2.do?app=user#?s=e2a369cd47dee5d08aba7f67536d4387&view=default&search=";
+    }
+  };
 // await authenticateWithServiceNow()
 }
 
