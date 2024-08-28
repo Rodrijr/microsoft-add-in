d08b321968df53b9d121d31f4c9854d47812e924
Jhonny Blanco <rodri_n@hotmail.es>
2024-08-28 16:33:45 -0400

    ebnscs

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -10,26 +10,35 @@ Office.onReady((info) => {
 });
 
 async function initialize() {
-  console.log('>>>>>>>>>>>>>>>>>>>>>>>initialize')
-  const el = document.createElement('iframe');
-  el.src = "https://iadbdev.service-now.com/login.do";
-  el.id = 'miIframe';
-  el.sandbox = "allow-scripts allow-forms allow-same-origin allow-popups allow-popups-to-escape-sandbox allow-modals allow-downloads allow-storage-access-by-user-activation";
-  el.className = "AddinIframe";
+  const serviceNowTokenEndpoint = 'https://iadbdev.service-now.com/oauth_token.do';
+  const clientId = process.env.TESTID;
+  console.log('JRBP -> clientId:', clientId);
+  const clientSecret = process.env.TEST;
+  console.log('JRBP -> clientSecret:', clientSecret);
+  const redirectUri = 'https://rodrijr.github.io/microsoft-add-in/src/taskpane/taskpane.html';
 
-  document.getElementById("miIframe")?.remove();
-  setTimeout(function () {
-      const user = document.getElementById("user_name");
-      user.value = 'autocad_integration';
+  fetch(serviceNowTokenEndpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: `grant_type=authorization_code&client_id=${clientId}&client_secret=${clientSecret}&code=${accessToken}&redirect_uri=${redirectUri}`
 
-      const pass = document.getElementById("user_password");
-      pass.value = 'AutoCadIntegration67=';
-
-      const button = document.getElementById("sysverb_login");
-      button.click();
-      iframeWindow.location.href = "https://iadbdev.service-now.com/x_nuvo_eam_fm_view_v2.do?app=user#?s=e2a369cd47dee5d08aba7f67536d4387&view=default&search=";
-  }, 5000); // 5000 milliseconds = 5 seconds
-  document.getElementById("preview").appendChild(el);
-
-// await authenticateWithServiceNow()
+  })
+    .then(response => response.json())
+    .then(data => {
+      const serviceNowAccessToken = data.access_token;
+      const iframeUrl = `https://iadbdev.service-now.com/x_nuvo_eam_fm_view_v2.do?app=user#?s=e2a369cd47dee5d08aba7f67536d4387&view=default&search=&sysparm_access_token=${serviceNowAccessToken}`;
+      var el = document.createElement("iframe");
+      el.src = iframeUrl;
+      el.id = 'miIframe';
+      el.sandbox = "allow-scripts allow-same-origin allow-top-navigation-by-user-activation";
+      el.referrerpolicy = "strict-origin-when-cross-origin";
+      el.setAttribute('Authorization', authToken);
+      var a = document.getElementById("miIframe")?.remove();
+      document.getElementById("preview").appendChild(el);
+    })
+    .catch(error => {
+      console.error('Error al obtener el token de ServiceNow:', error);
+    });
 }
