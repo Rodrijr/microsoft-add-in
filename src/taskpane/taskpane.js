35e9f1f3f7214f077d20dab1d8570a4959ef51b7
Jhonny Blanco <rodri_n@hotmail.es>
2024-08-29 08:41:24 -0400

    logintest

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -1,44 +1,27 @@
 /* global Office */
+import axios from 'axios';
 const locationEndpoint = 'https://iadbdev.service-now.com/x_nuvo_eam_microsoft_add_in.do?location=';
-
+const instance = axios.create({
+  baseURL: 'https://iadbdev.service-now.com',
+  timeout: 5000,
+});
 Office.onReady((info) => {
   if (info.host === Office.HostType.Outlook) {
-    initialize();
-
-
+    loginOAUTH();
   }
 });
 
-async function initialize() {
-  const serviceNowTokenEndpoint = 'https://iadbdev.service-now.com/oauth_token.do';
-  const clientId = process.env.TESTID;
-  console.log('JRBP -> clientId:', clientId);
-  const clientSecret = process.env.TEST;
-  console.log('JRBP -> clientSecret:', clientSecret);
-  const redirectUri = 'https://rodrijr.github.io/microsoft-add-in/src/taskpane/taskpane.html';
+async function loginOAUTH() {
+  try {
 
-  fetch(serviceNowTokenEndpoint, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    body: `grant_type=authorization_code&client_id=${clientId}&client_secret=${clientSecret}&code=${accessToken}&redirect_uri=${redirectUri}`
-
-  })
-    .then(response => response.json())
-    .then(data => {
-      const serviceNowAccessToken = data.access_token;
-      const iframeUrl = `https://iadbdev.service-now.com/x_nuvo_eam_fm_view_v2.do?app=user#?s=e2a369cd47dee5d08aba7f67536d4387&view=default&search=&sysparm_access_token=${serviceNowAccessToken}`;
-      var el = document.createElement("iframe");
-      el.src = iframeUrl;
-      el.id = 'miIframe';
-      el.sandbox = "allow-scripts allow-same-origin allow-top-navigation-by-user-activation";
-      el.referrerpolicy = "strict-origin-when-cross-origin";
-      el.setAttribute('Authorization', authToken);
-      var a = document.getElementById("miIframe")?.remove();
-      document.getElementById("preview").appendChild(el);
-    })
-    .catch(error => {
-      console.error('Error al obtener el token de ServiceNow:', error);
+    const instance = axios.create({
+      baseURL: 'https://iadbdev.service-now.com',
+      timeout: 5000,
     });
+    var resp = instance.post('/oauth_token.do?grant_type=password&client_id=f3600e11ee4bca94785814825f74d23a&client_secret=wUi%26mLGH0f&password=AutoCadIntegration67%3D&username=autocad_integration')
+    console.log('JRBP -> resp:', resp);
+
+  } catch (error) {
+    console.log('JRBP -> error:', error);
+  }
 }
