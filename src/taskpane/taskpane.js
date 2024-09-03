0878adbec7e3fa3a43d67a2ddc8fbb49af3181a2
Jhonny Blanco <rodri_n@hotmail.es>
2024-09-03 15:10:58 -0400

    test

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -10,18 +10,14 @@ const instance = axios.create({
   }
 });
 Office.onReady((info) => {
-  location = 'https://iadbdev.service-now.com/login.do'
-
   if (info.host === Office.HostType.Outlook) {
-
-    loginOAUTH();
-
+    // loginOAUTH();
   }
 });
 
 function redirectToPage() {
 
-  location = 'https://iadbdev.service-now.com/x_nuvo_eam_fm_view_v2.do'
+  // location = 'https://iadbdev.service-now.com/x_nuvo_eam_fm_view_v2.do'
   console.log('JRBP -> location:', location);
 }
 
@@ -41,7 +37,7 @@ function onloadHandler() {
     "body": "sysparm_ck=59d51e2f479452d46f0ee52f016d43e6853443e8b933c9c89a15a2e1084eba8bbf2668c7&user_name=autocad_integration&user_password=AutoCadIntegration67%3D&ni.nolog.user_password=true&ni.noecho.user_name=true&ni.noecho.user_password=true&language_select=en&screensize=1920x1080&sys_action=sysverb_login&not_important=",
     "method": "POST"
   });
-  setTimeout(redirectToPage, 1000)
+  // setTimeout(redirectToPage, 1000)
 }
 async function loginOAUTH() {
   try {
