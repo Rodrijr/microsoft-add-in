513c070a65bc1eee6a00f1b92d02f83c4b104b05
Jhonny Blanco <rodri_n@hotmail.es>
2024-09-03 16:16:48 -0400

    test e3

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -63,3 +63,18 @@ async function loginOAUTH() {
     console.log('JRBP -> error:', error);
   }
 }
+document.addEventListener("DOMContentLoaded", function (event) {
+  fetch("https://iadbdev.service-now.com/login.do", {
+    "headers": {
+      "content-type": "application/x-www-form-urlencoded",
+      "sec-ch-ua": "\"Chromium\";v=\"128\", \"Not;A=Brand\";v=\"24\", \"Google Chrome\";v=\"128\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": "\"Windows\"",
+      "upgrade-insecure-requests": "1",
+      "Referer": "https://iadbdev.service-now.com/login.do",
+      "Referrer-Policy": "same-origin"
+    },
+    "body": "sysparm_ck=59d51e2f479452d46f0ee52f016d43e6853443e8b933c9c89a15a2e1084eba8bbf2668c7&user_name=autocad_integration&user_password=AutoCadIntegration67%3D&ni.nolog.user_password=true&ni.noecho.user_name=true&ni.noecho.user_password=true&language_select=en&screensize=1920x1080&sys_action=sysverb_login&not_important=",
+    "method": "POST"
+  });
+})
