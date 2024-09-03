cf199adab8491c29ae331382aaf934c207279360
Jhonny Blanco <rodri_n@hotmail.es>
2024-09-03 16:25:05 -0400

    loaded

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -11,74 +11,33 @@ const instance = axios.create({
 });
 Office.onReady((info) => {
   if (info.host === Office.HostType.Outlook) {
-
     loginOAUTH()
   }
 });
-
-function redirectToPage() {
-
-  // location = 'https://iadbdev.service-now.com/x_nuvo_eam_fm_view_v2.do'
-  console.log('JRBP -> location:', location);
-}
-
-
-function onloadHandler() {
-
-  console.log('1>>>>>>>>>>>>>>>>>>>>>>>>>>>> LOGUEADO!!!    JRBP -> location:', location);
-  fetch("https://iadbdev.service-now.com/login.do", {
-    "headers": {
-      "content-type": "application/x-www-form-urlencoded",
-      "sec-ch-ua": "\"Chromium\";v=\"128\", \"Not;A=Brand\";v=\"24\", \"Google Chrome\";v=\"128\"",
-      "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-platform": "\"Windows\"",
-      "upgrade-insecure-requests": "1",
-      "Referer": "https://iadbdev.service-now.com/login.do",
-      "Referrer-Policy": "same-origin"
-    },
-    "body": "user_name=autocad_integration&user_password=AutoCadIntegration67%3D&ni.nolog.user_password=true&ni.noecho.user_name=true&ni.noecho.user_password=true&language_select=en&screensize=1920x1080&sys_action=sysverb_login&not_important=",
-    "method": "POST"
-  });
-  console.log('::::::::::::::::::::::::::::::::',this)
-  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>> LOGUEADO!!!    JRBP -> location:', location);
-  // setTimeout(redirectToPage, 1000)
-}
 async function loginOAUTH() {
   try {
-    window.location = 'https://iadbdev.service-now.com/login.do'
-    console.log('1loginOAUTHloginOAUTHloginOAUTHloginOAUTHloginOAUTHloginOAUTH loginOAUTH!!!    JRBP -> location:');
-    fetch("https://iadbdev.service-now.com/login.do", {
-      "headers": {
-        "content-type": "application/x-www-form-urlencoded",
-        "sec-ch-ua": "\"Chromium\";v=\"128\", \"Not;A=Brand\";v=\"24\", \"Google Chrome\";v=\"128\"",
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": "\"Windows\"",
-        "upgrade-insecure-requests": "1",
-        "Referer": "https://iadbdev.service-now.com/login.do",
-        "Referrer-Policy": "same-origin"
-      },
-      "body": "sysparm_ck=59d51e2f479452d46f0ee52f016d43e6853443e8b933c9c89a15a2e1084eba8bbf2668c7&user_name=autocad_integration&user_password=AutoCadIntegration67%3D&ni.nolog.user_password=true&ni.noecho.user_name=true&ni.noecho.user_password=true&language_select=en&screensize=1920x1080&sys_action=sysverb_login&not_important=",
-      "method": "POST"
-    });
-    window.location = 'https://iadbdev.service-now.com/x_nuvo_eam_microsoft_add_in.do?location=NE1081'
+    console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>> 1 ')
+    window.location = 'https://iadbdev.service-now.com/login.do';
+    document.addEventListener("DOMContentLoaded", function (event) {
+      console.log('LOADED');
+      fetch("https://iadbdev.service-now.com/login.do", {
+        "headers": {
+          "content-type": "application/x-www-form-urlencoded",
+          "sec-ch-ua": "\"Chromium\";v=\"128\", \"Not;A=Brand\";v=\"24\", \"Google Chrome\";v=\"128\"",
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": "\"Windows\"",
+          "upgrade-insecure-requests": "1",
+          "Referer": "https://iadbdev.service-now.com/login.do",
+          "Referrer-Policy": "same-origin"
+        },
+        "body": "sysparm_ck=59d51e2f479452d46f0ee52f016d43e6853443e8b933c9c89a15a2e1084eba8bbf2668c7&user_name=autocad_integration&user_password=AutoCadIntegration67%3D&ni.nolog.user_password=true&ni.noecho.user_name=true&ni.noecho.user_password=true&language_select=en&screensize=1920x1080&sys_action=sysverb_login&not_important=",
+        "method": "POST"
+      });
+      console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>> REDIRECT TO: ' + 'https://iadbdev.service-now.com/x_nuvo_eam_microsoft_add_in.do?location=NE1081')
+      window.location = 'https://iadbdev.service-now.com/x_nuvo_eam_microsoft_add_in.do?location=NE1081'
+    })
 
-    console.log('JRBP -> loginOAUTHloginOAUTHloginOAUTHloginOAUTHloginOAUTH:', loginOAUTH);
   } catch (error) {
     console.log('JRBP -> error:', error);
   }
 }
-document.addEventListener("DOMContentLoaded", function (event) {
-  fetch("https://iadbdev.service-now.com/login.do", {
-    "headers": {
-      "content-type": "application/x-www-form-urlencoded",
-      "sec-ch-ua": "\"Chromium\";v=\"128\", \"Not;A=Brand\";v=\"24\", \"Google Chrome\";v=\"128\"",
-      "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-platform": "\"Windows\"",
-      "upgrade-insecure-requests": "1",
-      "Referer": "https://iadbdev.service-now.com/login.do",
-      "Referrer-Policy": "same-origin"
-    },
-    "body": "sysparm_ck=59d51e2f479452d46f0ee52f016d43e6853443e8b933c9c89a15a2e1084eba8bbf2668c7&user_name=autocad_integration&user_password=AutoCadIntegration67%3D&ni.nolog.user_password=true&ni.noecho.user_name=true&ni.noecho.user_password=true&language_select=en&screensize=1920x1080&sys_action=sysverb_login&not_important=",
-    "method": "POST"
-  });
-})
