da1094a6f72d5eb56eba5aabec1d5eccf1b526f8
Jhonny Blanco <rodri_n@hotmail.es>
2024-09-03 10:54:14 -0400

    using iframe login do

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -3,10 +3,10 @@ const locationEndpoint = 'https://iadbdev.service-now.com/x_nuvo_eam_microsoft_a
 const instance = axios.create({
   baseURL: 'https://iadbdev.service-now.com',
   timeout: 5000,
-  headers: {
+    headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*'
+    'Authorization': 'Basic ' + btoa('autocad_integration' + ':' + 'AutoCadIntegration67=')
   }
 });
 Office.onReady((info) => {
@@ -14,7 +14,16 @@ Office.onReady((info) => {
     loginOAUTH();
   }
 });
+function onloadHandler() {
 
+  console.log("ESTOY EN EL onloadHandler?????????????????????????????????????")
+  var a = document.getElementById('user_name')
+  console.log(a)
+}
+document.addEventListener("DOMContentLoaded", function (event) {
+
+  console.log("cargo la pagina")
+})
 async function loginOAUTH() {
   try {
     var resp = await instance.post('/oauth_token.do?grant_type=password&client_id=f3600e11ee4bca94785814825f74d23a&client_secret=wUi%26mLGH0f&password=AutoCadIntegration67%3D&username=autocad_integration')
