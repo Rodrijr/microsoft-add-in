7f98109ad45f6d838e703db50bc20b43bc3f3205
Jhonny Blanco <rodri_n@hotmail.es>
2024-09-04 16:29:16 -0400

    then

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -10,7 +10,7 @@ Office.onReady((info) => {
       if (location == 'https://iadbdev.service-now.com/login.do' && count < 5) {
         console.log('HACER FETCH 1')
         count++;
-        var resp = await fetch("https://iadbdev.service-now.com/login.do", {
+        fetch("https://iadbdev.service-now.com/login.do", {
           "headers": {
             "content-type": "application/x-www-form-urlencoded",
             "sec-ch-ua": "\"Chromium\";v=\"128\", \"Not;A=Brand\";v=\"24\", \"Google Chrome\";v=\"128\"",
@@ -22,14 +22,14 @@ Office.onReady((info) => {
           },
           "body": "sysparm_ck=59d51e2f479452d46f0ee52f016d43e6853443e8b933c9c89a15a2e1084eba8bbf2668c7&user_name=autocad_integration&user_password=AutoCadIntegration67%3D&ni.nolog.user_password=true&ni.noecho.user_name=true&ni.noecho.user_password=true&language_select=en&screensize=1920x1080&sys_action=sysverb_login&not_important=",
           "method": "POST"
+        }).then((resp) => {
+          console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>', resp)
+          console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>1111', location)
+          location = 'https://iadbdev.service-now.com/login.do'
+          console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>22222', location)
         });
-        location = 'https://iadbdev.service-now.com/login.do'
 
       }
     }
-    setInterval(fetchFunction, 1000)
-    setTimeout(function () {
-      location ='https://iadbdev.service-now.com/login.do'
-    }, 1500)
   }
 });
